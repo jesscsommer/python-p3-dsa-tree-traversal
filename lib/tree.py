89c7e0e6e882e6079a3beec24fb3aea6943d6c97
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-30 10:29:55 -0700

    Complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,23 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root = None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    # Depth first 
+    # def get_element_by_id(self, id):
+    #     nodes_to_vist = [self.root]
+    #     while len(nodes_to_vist) > 0:
+    #         node = nodes_to_vist.pop(0)
+    #         if node["id"] == id: 
+    #             return node
+    #         nodes_to_vist = node["children"] + nodes_to_vist
+    #     return None
+
+    # Breadth first 
+    def get_element_by_id(self, id):
+        nodes_to_vist = [self.root]
+        while len(nodes_to_vist) > 0:
+            node = nodes_to_vist.pop(0)
+            if node["id"] == id: 
+                return node
+            nodes_to_vist = nodes_to_vist + node["children"]
+        return None
